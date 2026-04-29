818afad596d1638cba82edba047786c8aa70113e
Blacknira <149145592+Blacknira@users.noreply.github.com>
2026-04-29 14:34:07 +0530

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,88 @@
+// Task 1: variable update
+var num = 50;
+num=100;
+console.log(num);
+
+// Task 2 : Variable math
+
+let marks = 80;
+marks=marks+10;
+console.log(marks);
+
+// Task 3 : Const value
+
+const price = 500;
+let finalprice = price + 100;
+console.log(finalprice);
+
+// Task 4 : Printing Statement
+
+ console.log("Welcome Team");
+ console.log(2026);
+ console.log(true);
+
+// Task 5 : datatype check
+
+console.log(typeof("Javascript"));
+console.log(typeof(250));
+console.log(typeof(false));
+
+// Task 6 : Array task
+
+let fruits = ["Apple", "Banana", "Papaya", "Pineapple", "mango"];
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[4]);
+
+// Task 7 : object task
+
+let student ={
+    name : "Niranjana", 
+    age : 22,
+    course : "BSc Computer Science"
+}
+
+console.log(student.name);
+console.log(student.age);
+
+// Task 8 : Arithmetic operations
+
+console.log(20+10);
+console.log(50-25);
+console.log(5*5);
+console.log(100/4);
+console.log(20%3);
+
+// task 9 : Increment and Decrement
+
+let x = 5;
+x++;
+console.log(x);
+
+let y = 10;
+y--;
+console.log(y);
+
+// Task 10 : comparison operators
+
+console.log(10>5);
+console.log(5<2);
+console.log(20=="20");
+console.log(15==="15");
+console.log(10!=8);
+
+// Task 11 : logical operators
+
+console.log(5>2 && 10>3);
+console.log(7<5 || 10<3);
+console.log(!(10>5));
+
+// Task 12 : if-else statement
+
+let age = 18;
+
+if (age >= 18) {
+    console.log("Eligible to vote.");
+} else {
+    console.log("Not eligible to vote.");
+}
---
+++ Task3.js
@@ -0,0 +1,168 @@
+// Level 1 – Functions Basics
+
+// Task 1: Student Form Function
+function studentForm(name, age, course) {
+    console.log("Name:", name);
+    console.log("Age:", age);
+    console.log("Course:", course);
+}
+studentForm("Naveen", 22, "MERN");
+
+
+
+
+// Task 2: Calculator Function
+function calc(a, b) {
+    console.log("Addition:", a + b);
+    console.log("Subtraction:", a - b);
+    console.log("Multiplication:", a * b);
+}
+calc(10, 5);
+
+
+
+
+// Task 3: Reusable Greeting
+function greet(name) {
+    console.log("Hello", name);
+}
+greet("Kamal");
+greet("Praveen");
+greet("Sai");
+
+
+
+
+
+// Level 2 – Return & Scope
+
+// Task 4: Return Value
+function square(num) {
+    return num * num;
+}
+console.log(square(5));
+
+
+
+
+// Task 5: Scope Check
+function testScope() {
+    let secret = "javascript";
+}
+testScope();
+
+console.log(secret);  
+
+
+
+
+
+
+//  Level 3 – Spread / Rest
+
+// Task 6: Merge Arrays
+let boys = ["car","bike"];
+let girls = ["doll","teddy"];
+
+let combined = [...boys, ...girls];
+console.log(combined);
+
+
+
+// Task 7: Unlimited Numbers
+function sumAll(...nums) {
+    let sum = 0;
+    for (let num of nums) {
+        sum += num;
+    }
+    console.log(sum);
+}
+sumAll(10,20,30,40);
+
+
+
+
+
+//  Level 4 – Destructuring
+
+// Task 8: Array Destructuring
+let colors = ["red","green","blue"];
+let [c1, c2, c3] = colors;
+
+console.log(c1, c2, c3);
+
+
+
+// Task 9: Object Destructuring
+let emp = {
+    name:"Naveen",
+    role:"Developer",
+    salary:"5LPA"
+};
+
+let {name, role} = emp;
+console.log(name, role);
+
+
+
+
+
+//  Level 5 – Real-Time Logic
+
+// Task 10: Offer Generator
+function* offerGenerator() {
+    yield "10% cashback";
+    yield "20% cashback";
+    yield "50% cashback";
+    yield "Try again";
+}
+
+let offer = offerGenerator();
+
+console.log(offer.next().value);
+console.log(offer.next().value);
+console.log(offer.next().value);
+console.log(offer.next().value);
+console.log(offer.next().value);
+
+
+
+
+//  Level 6 – Advanced
+
+// Task 11: Curry Function
+function add(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c;
+        }
+    }
+}
+console.log(add(10)(20)(30));
+
+
+
+
+// Task 12: Student Marks Analyzer
+function marks(...nums) {
+    let total = nums.reduce((acc, val) => acc + val, 0);
+    let avg = total / nums.length;
+
+    console.log("Total =", total);
+    console.log("Average =", avg);
+}
+marks(80,90,70,60);
+
+
+
+
+//  Challenge Task – Employee Registration
+
+function register(name, role, ...skills) {
+    console.log("Name:", name);
+    console.log("Role:", role);
+    console.log("Skills:", skills.join(" "));
+}
+
+register("Naveen","Frontend","HTML","CSS","JS","React");
+
